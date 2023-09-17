3504a95b68b5be7a5077f0cd44ffc847d7471af4
Léonard Jouve <leonard.jouve@gmail.com>
2023-09-17 19:56:25 +0200

    CORS

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/LeonardJouve/task-board-api/api"
 	"github.com/LeonardJouve/task-board-api/auth"
 	"github.com/LeonardJouve/task-board-api/dotenv"
 	"github.com/LeonardJouve/task-board-api/store"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -25,24 +25,18 @@ func main() {
 	}
 
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     fmt.Sprintf("http://%s:%s", os.Getenv("HOST"), os.Getenv("PORT")),
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowMethods:     "GET, POST, PUT, PATCH, DELETE",
+		AllowCredentials: true,
+	}))
 
-	app.All("/*", router)
+	app.All("/api/*", auth.Protect, api.Router)
+	app.All("/auth/*", auth.Router)
 
 	err = app.Listen(fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT")))
 	if err != nil {
 		panic(err.Error())
-	}
-}
-
-func router(c *fiber.Ctx) error {
-	switch strings.Split(c.Path(), "/")[1] {
-	case "api":
-		return api.Router(c)
-	case "auth":
-		return auth.Router(c)
-	default:
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"message": "not found",
-		})
 	}
 }
