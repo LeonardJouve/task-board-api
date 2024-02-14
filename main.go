abca55985c05e52ff068dc4c3efec60cd7c3d996
Leonard <leonard.jouve@gmail.com>
2024-02-14 12:35:51 +0100

    add /api group

--- main.go
+++ main.go
@@ -48,6 +48,8 @@ func main() {
 
 	app := fiber.New()
 
+	apiGroup := app.Group("/api")
+
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     "https://localhost:5173", // TODO
 		AllowHeaders:     "Origin, Content-Type, Accept, X-CSRF-Token, Authorization",
@@ -83,24 +85,24 @@ func main() {
 		panic(err.Error())
 	}
 
-	app.Static("/assets", assetsPath)
+	apiGroup.Static("/assets", assetsPath)
 
 	// /ws
 	go websocket.Process()
-	app.Get("/ws", auth.Protect, websocket.HandleUpgrade, websocket.HandleSocket)
+	apiGroup.Get("/ws", auth.Protect, websocket.HandleUpgrade, websocket.HandleSocket)
 
 	// /auth
-	authGroup := app.Group("/auth")
+	authGroup := apiGroup.Group("/auth")
 	authGroup.Post("/register", auth.Register)
 	authGroup.Post("/login", auth.Login)
 	authGroup.Get("/refresh", auth.Refresh)
 	authGroup.Get("/logout", auth.Logout)
 	authGroup.Get("/csrf", auth.GetCSRF)
 
-	apiGroup := app.Group("/api", auth.Protect)
+	restGroup := apiGroup.Group("/rest", auth.Protect)
 
 	// /api/boards
-	boardsGroup := apiGroup.Group("/boards")
+	boardsGroup := restGroup.Group("/boards")
 	boardsGroup.Get("/", api.GetBoards)
 	boardsGroup.Get("/:board_id", api.GetBoard)
 	boardsGroup.Get("/:board_id/invite", api.InviteBoard)
@@ -110,7 +112,7 @@ func main() {
 	boardsGroup.Delete("/:board_id", api.DeleteBoard)
 
 	// /api/columns
-	columnsGroup := apiGroup.Group("/columns")
+	columnsGroup := restGroup.Group("/columns")
 	columnsGroup.Get("/", api.GetColumns)
 	columnsGroup.Get("/:column_id", api.GetColumn)
 	columnsGroup.Post("/", api.CreateColumn)
@@ -119,7 +121,7 @@ func main() {
 	columnsGroup.Delete("/:column_id", api.DeleteColumn)
 
 	// /api/cards
-	cardsGroup := apiGroup.Group("/cards")
+	cardsGroup := restGroup.Group("/cards")
 	cardsGroup.Get("/", api.GetCards)
 	cardsGroup.Get("/:card_id", api.GetCard)
 	cardsGroup.Get("/:card_id/join", api.JoinCard)
@@ -132,7 +134,7 @@ func main() {
 	cardsGroup.Delete("/:card_id", api.DeleteCard)
 
 	// /api/tags
-	tagsGroup := apiGroup.Group("/tags")
+	tagsGroup := restGroup.Group("/tags")
 	tagsGroup.Get("/", api.GetTags)
 	tagsGroup.Get("/:tag_id", api.GetTag)
 	tagsGroup.Post("/", api.CreateTag)
@@ -140,7 +142,7 @@ func main() {
 	tagsGroup.Delete("/:tag_id", api.DeleteTag)
 
 	// /api/users
-	usersGroup := apiGroup.Group("/users")
+	usersGroup := restGroup.Group("/users")
 	usersGroup.Get("/me", api.GetMe)
 	usersGroup.Get("/", api.GetUsers)
 	usersGroup.Get("/:user_id", api.GetUser)
